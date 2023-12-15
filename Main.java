f20330b90ad677b194782556ccb0972fb27c2e95
lbanela <banella.lorenzo@gmail.com>
2023-12-15 14:31:09 +0000

    modifiche main

--- Main.java
+++ Main.java
@@ -1,23 +1,50 @@
 import java.util.ArrayList;
+import java.util.Scanner;  // Import the Scanner class
+
+/*
+chiedere la parola segreta 
+-scanner
+-buffered
+
+-chiedere la frase in chiaro
+-cifrare la frase
+-stampare la frase cifrata
+
+
+*/
+
+
 class Main {
   public static void main(String[] args) {
     //SALUTO! 
      ArrayList<Vigenere> quadranti = new ArrayList<Vigenere>();
 
     System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
-    Matrice s=new Matrice("TPSIT");
+    Scanner scanner = new Scanner(System.in);  
+    System.out.println("Inserisci  il verme");
+
+    String verme = scanner.nextLine().toUpperCase(); 
+
+
+    System.out.println("Inserisci  la parola in chiaro");
+
+    String parola_in_chiaro = scanner.nextLine().toUpperCase(); 
+
     
-    Vigenere quadrante_1=new Vigenere(0,13,0,13,s);
+    Matrice matrix=new Matrice(verme);
+    
+    Vigenere quadrante_1=new Vigenere(0,12,0,12,matrix);
      quadranti.add(quadrante_1);
     
-    Vigenere quadrante_2=new Vigenere(0,13,13,26,s);
+    Vigenere quadrante_2=new Vigenere(0,12,12,26,matrix);
      quadranti.add(quadrante_2);
     
-    Vigenere quadrante_3=new Vigenere(13,26,0,13,s);
+    Vigenere quadrante_3=new Vigenere(12,26,0,12,matrix);
      quadranti.add(quadrante_3);
     
-    Vigenere quadrante_4=new Vigenere(13,26,13,26,s);
+    Vigenere quadrante_4=new Vigenere(12,26,12,26,matrix);
     quadranti.add(quadrante_4);
+
     
     for(Vigenere v:quadranti){
      Thread t= new Thread(v);
@@ -30,6 +57,11 @@ class Main {
     }  
     
       
-   s.stampa();
+    matrix.stampa();
+
+    String fraseCriptata=matrix.cifra(parola_in_chiaro);
+    System.out.println("La frase criptata è "+fraseCriptata);
+    scanner.close();
+
   }
 }
